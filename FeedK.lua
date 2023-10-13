2d3143a1be02834a7de33842b102eb011c4fea90
Marcio Carola Nunes Filho <marcio8filho@outlook.com>
2023-10-13 01:30:46 -0300

    Update FeedK.lua

--- FeedK.lua
+++ FeedK.lua
@@ -131,9 +131,10 @@ function MontaOResto()
                             -- print(meumanevalornum,valornum)
 
                             if(meumanevalornum>=valornum or osbuto[i].Name == "Insert Food")then
+                                print("Comprando:",osbuto[i])
                                 local Obodao = config.meuplot:WaitForChild("Buttons"):WaitForChild(osbuto[i].Name)
                                 if(#Obodao:GetChildren()>1)then
-                                    print("Comprando:",osbuto[i])
+                                    print(Obodao.Name)
                                     local pressiona = Obodao.Presser
                                     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = pressiona.CFrame
                                 end
@@ -188,6 +189,7 @@ function PegaPlot()
                 if not(oiugatu=="C/:") then
                     config.meuplot = osplot[i].Plot
                     pair:Hide()
+                    game.Players.LocalPlayer.PlayerGui.MainHud.Boards:WaitForChild("Insert Food").Price.Text = "$0"
                     MontaOResto()
                 end
             end
