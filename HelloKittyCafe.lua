d5e21a27f54ccfdd35ccd9918522bdcba8ee8c97
Marcio Carola Nunes Filho <marcio8filho@outlook.com>
2023-10-14 19:22:33 -0300

    Update HelloKittyCafe.lua

--- HelloKittyCafe.lua
+++ HelloKittyCafe.lua
@@ -395,7 +395,7 @@ if(Kuromi)then
         if(config.pushabolo)then
             for i=1,#game.Players:GetChildren() do
                 if(game.Players:GetChildren()[i].Character.Humanoid.WalkSpeed==6 and game.Players:GetChildren()[i] ~= game.Players.LocalPlayer) then
-                    game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored=true
+                    game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored=false
                     game.Players:GetChildren()[i].Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
                 end
             end
