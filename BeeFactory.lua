e02a7022ae0513f883ca07913477fb580d8bdac1
Marcio Carola Nunes Filho <marcio8filho@outlook.com>
2023-10-16 15:58:15 -0300

    Update BeeFactory.lua

--- BeeFactory.lua
+++ BeeFactory.lua
@@ -19,7 +19,9 @@ local config = {
     entregar = false,
     upabieia = false,
     maitaxa = false,
-    afkdelay = 1000
+    afkdelay = 1000,
+    mashupa = false,
+    antiafk = false
 }
 print((tonumber("3")), tonumber("3.2"))
 local Library = initLibrary()
@@ -44,16 +46,20 @@ Inicio:Toggle{
         config.upabieia = oq
         config.melzin = oq
         config.maitaxa = oq
+        config.mashupa = oq
+        config.antiafk = oq
     end
 }
 
 
 game:GetService("RunService").RenderStepped:Connect(function()
-    if(config.afkdelay>0)then
-        config.afkdelay -= 1
-    else
-        config.afkdelay = 1000
-        input.press(Enum.KeyCode.Space)
+    if(config.antiafk)then
+        if(config.afkdelay>0)then
+            config.afkdelay -= 1
+        else
+            config.afkdelay = 1000
+            input.press(Enum.KeyCode.Space)
+        end
     end
     if(config.melzin)then
         local osmel = game.Workspace.Tycoons:WaitForChild(game.Players.LocalPlayer.Name).Honey:GetChildren()
@@ -95,50 +101,105 @@ game:GetService("RunService").RenderStepped:Connect(function()
             end
         end
         if(uprecinnum<=mamaninum)then
-            print("Comprei Abeia!")
             local UbutoPaPagah = game.Workspace.Tycoons:WaitForChild(game.Players.LocalPlayer.Name).Buttons.Purchase_Bee
             firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart,UbutoPaPagah,0)
             firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart,UbutoPaPagah,1)
         end
     end
     if(config.maitaxa)then
-        local uprecinstr = game.Workspace.Tycoons:WaitForChild(game.Players.LocalPlayer.Name).BillboardGuis.UpgradeSell.BillboardGui.Cost.Text
-        uprecinstr = uprecinstr:gsub("%,", "")
-        local Modnum = string.sub(uprecinstr,#uprecinstr,#uprecinstr)
-        local uprecinnum = nil
+        local uprecinstr2 = game.Workspace.Tycoons:WaitForChild(game.Players.LocalPlayer.Name).BillboardGuis.SellPartText.BillboardGui.Amount.Text
+        uprecinstr2 = uprecinstr2:gsub("%,", "")
+        local Modnum = string.sub(uprecinstr2,#uprecinstr2,#uprecinstr2)
+        local uprecinnum2 = nil
         if(Modnum=="K")then
-            uprecinnum = tonumber(string.sub(uprecinstr,2,#uprecinstr-1))*1000
+            uprecinnum2 = tonumber(string.sub(uprecinstr2,1,#uprecinstr2-1))*1000
         else
             if(Modnum=="M") then
-                uprecinnum = tonumber(string.sub(uprecinstr,2,#uprecinstr-1))*1000000
+                uprecinnum2 = tonumber(string.sub(uprecinstr2,1,#uprecinstr2-1))*1000000
             else
-                uprecinnum = tonumber(string.sub(uprecinstr,2,#uprecinstr))
-            end
-        end
-        print(uprecinnum)
-
-        local mamanistr = game.Players.LocalPlayer.PlayerGui.ScreenGui.Frame.Coins.TextLabel.Text
-        mamanistr = mamanistr:gsub("%,", "")
-        Modnum = string.sub(mamanistr,#mamanistr,#mamanistr)
-        local mamaninum = nil
-        if(Modnum=="K")then
-            mamaninum = tonumber(string.sub(mamanistr,2,#mamanistr-1))*1000
-        else
-            if(Modnum=="M") then
-                mamaninum = tonumber(string.sub(mamanistr,2,#mamanistr-1))*1000000
-            else
-                mamaninum = tonumber(string.sub(mamanistr,2,#mamanistr))
+                uprecinnum2 = tonumber(string.sub(uprecinstr2,1,#uprecinstr2))
             end
         end
 
-        if(uprecinnum<=mamaninum)then
-            print("Comprei Taxa!")
-            local UbutoPaPagah = game.Workspace.Tycoons:WaitForChild(game.Players.LocalPlayer.Name).Buttons.Faster_Button
-            firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart,UbutoPaPagah,0)
-            firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart,UbutoPaPagah,1)
+        local uprecinstr3 = game.Workspace.Tycoons:WaitForChild(game.Players.LocalPlayer.Name).BillboardGuis.SellPartText.BillboardGui.Rate.Text
+        uprecinstr3 = uprecinstr3:split(" ")[2]
+        uprecinstr3 = uprecinstr3:gsub("%/s", "")
+        uprecinstr3 = uprecinstr3:gsub("%,", "")
+        Modnum = string.sub(uprecinstr3,#uprecinstr3,#uprecinstr3)
+        local uprecinnum3 = nil
+        if(Modnum=="K")then
+            uprecinnum3 = tonumber(string.sub(uprecinstr3,1,#uprecinstr3-1))*1000
+        else
+            if(Modnum=="M") then
+                uprecinnum3 = tonumber(string.sub(uprecinstr3,1,#uprecinstr3-1))*1000000
+            else
+                uprecinnum3 = tonumber(string.sub(uprecinstr3,1,#uprecinstr3))
+            end
+        end
+
+        if(uprecinnum2>=uprecinnum3)then
+            local uprecinstr = game.Workspace.Tycoons:WaitForChild(game.Players.LocalPlayer.Name).BillboardGuis.UpgradeSell.BillboardGui.Cost.Text
+            uprecinstr = uprecinstr:gsub("%,", "")
+            Modnum = string.sub(uprecinstr,#uprecinstr,#uprecinstr)
+            local uprecinnum = nil
+            if(Modnum=="K")then
+                uprecinnum = tonumber(string.sub(uprecinstr,2,#uprecinstr-1))*1000
+            else
+                if(Modnum=="M") then
+                    uprecinnum = tonumber(string.sub(uprecinstr,2,#uprecinstr-1))*1000000
+                else
+                    uprecinnum = tonumber(string.sub(uprecinstr,2,#uprecinstr))
+                end
+            end
+            local mamanistr = game.Players.LocalPlayer.PlayerGui.ScreenGui.Frame.Coins.TextLabel.Text
+            mamanistr = mamanistr:gsub("%,", "")
+            Modnum = string.sub(mamanistr,#mamanistr,#mamanistr)
+            local mamaninum = nil
+            if(Modnum=="K")then
+                mamaninum = tonumber(string.sub(mamanistr,2,#mamanistr-1))*1000
+            else
+                if(Modnum=="M") then
+                    mamaninum = tonumber(string.sub(mamanistr,2,#mamanistr-1))*1000000
+                else
+                    mamaninum = tonumber(string.sub(mamanistr,2,#mamanistr))
+                end
+            end
+
+            if(uprecinnum<=mamaninum)then
+                local UbutoPaPagah = game.Workspace.Tycoons:WaitForChild(game.Players.LocalPlayer.Name).Buttons.Faster_Button
+                firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart,UbutoPaPagah,0)
+                firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart,UbutoPaPagah,1)
+            end
         end
     end
-
+    if(config.mashupa)then
+        local cumea = game.Workspace.Tycoons:WaitForChild(game.Players.LocalPlayer.Name).Hive:GetChildren()
+        local asabeias = {}
+        local tiermax = 50
+        for preparano=1, tiermax do
+            table.insert(asabeias, tonumber(preparano), 0)
+        end
+        for i=1,#cumea do
+            if(#cumea[i]:GetChildren()>3)then
+                local cadeotier = cumea[i].Bee:GetChildren()
+                local peraprr = 0
+                for i=1,#cadeotier do
+                    if(#cadeotier[i]:GetChildren()>0 and cadeotier[i].ClassName == "MeshPart") then
+                        local otier = tonumber(cadeotier[i].TierText.Frame.Tier.Text:split(" ")[2])
+                        asabeias[otier] += 1
+                    end
+                end
+            end
+        end
+        for i=1,#asabeias do
+            if(asabeias[i]>=3)then
+                local merjador = game.Workspace.Tycoons:WaitForChild(game.Players.LocalPlayer.Name).Buttons.MergeBees
+                firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart,merjador,0)
+                firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart,merjador,1)
+                break
+            end
+        end
+    end
 end)
 
 Inicio:Button{
@@ -148,6 +209,8 @@ Inicio:Button{
         config.entregar = false
         config.upabieia = false
         config.maitaxa = false
+        config.mashupa = false
+        config.antiafk = false
         KillMe()
     end
 }
