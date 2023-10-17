5cd3b2200cd71ec7928ad8fd316d3f3a418cf1eb
Marcio Carola Nunes Filho <marcio8filho@outlook.com>
2023-10-17 01:41:16 -0300

    Update BeeFactory.lua

--- BeeFactory.lua
+++ BeeFactory.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/1uaxx/yunv2/main/librarysource.lua'))()
+-- loadstring(game:HttpGet('https://raw.githubusercontent.com/1uaxx/yunv2/main/librarysource.lua'))()
 local input = loadstring(game:HttpGet('https://pastebin.com/raw/dYzQv3d8'))()
 function GetObject(Astring)
     local ospedaco = Astring:split(".")
@@ -24,33 +24,6 @@ local config = {
     antiafk = false
 }
 print((tonumber("3")), tonumber("3.2"))
-local Library = initLibrary()
-local Window = Library:Load({
-    name = "Super Hack do Niquinistiquiniquis pro Krotoo",
-    sizeX = 425,
-    sizeY = 165,
-    color = Color3.fromRGB(255, 255, 255)
-})
-function  KillMe()
-    Window:Hide()
-end
-
-local Tab = Window:Tab("Bee Factoryyyy")
-local Inicio = Tab:Section { name = "Inicio"}
-
-Inicio:Toggle{
-    Name="Auto Farm",
-    flag="asdasd",
-    callback=function(oq)
-        config.entregar = oq
-        config.upabieia = oq
-        config.melzin = oq
-        config.maitaxa = oq
-        config.mashupa = oq
-        config.antiafk = oq
-    end
-}
-
 
 game:GetService("RunService").RenderStepped:Connect(function()
     if(config.antiafk)then
@@ -202,15 +175,173 @@ game:GetService("RunService").RenderStepped:Connect(function()
     end
 end)
 
-Inicio:Button{
-    Name = "Fechar UI",
-    callback = function()
-        config.melzin = false
+function CriarJanelaBase(Largura,Altura,TomPrimario)
+    local h = Instance.new("ScreenGui")
+    local Main = Instance.new("ImageLabel")
+    local Top = Instance.new("Frame")
+    local Title = Instance.new("TextLabel")
+    local ContainerFunc = Instance.new("Frame")
+    local TopContainerFunc = Instance.new("Frame")
+    local TitleJogo = Instance.new("TextLabel")
+
+    local modpertom = 8/255
+    local TomSecundario = Color3.new(TomPrimario.R+modpertom,TomPrimario.G+modpertom,TomPrimario.B+modpertom)
+    local TomTerciario = Color3.new(TomSecundario.R+modpertom,TomSecundario.G+modpertom,TomSecundario.B+modpertom)
+    local TomQuaternario = Color3.new(TomTerciario.R+modpertom,TomTerciario.G+modpertom,TomTerciario.B+modpertom)
+    local TomQuintenario = Color3.new(TomQuaternario.R+modpertom,TomQuaternario.G+modpertom,TomQuaternario.B+modpertom)
+    local TomSextenario = Color3.new(TomQuintenario.R+modpertom,TomQuintenario.G+modpertom,TomQuintenario.B+modpertom)
+
+    h.Name = "NixtiscriptEsteveAquiiiUwU"
+    h.Parent = game:GetService("CoreGui")
+    h.ResetOnSpawn = false
+
+    Main.Name = "Main"
+    Main.Parent = h
+    Main.Active = true
+    Main.Draggable = true
+    Main.BackgroundColor3 = TomTerciario
+    Main.BorderSizePixel = 1
+    Main.BorderColor3 = Color3.fromRGB(10, 10, 10)
+    Main.Position = UDim2.new(0, 0, 0, 0)
+    Main.Size = UDim2.new(0, Largura, 0, Altura)
+
+    Top.Name = "TituloContainer"
+    Top.Parent = Main
+    Top.BackgroundColor3 = TomPrimario
+    Top.BorderSizePixel = 1
+    Top.BorderColor3 = Color3.fromRGB(10, 10, 10)
+    Top.Position = UDim2.new(0, 0, 0, 0)
+    Top.Size = UDim2.new(0, Largura, 0, 25)    
+  
+    Title.Name = "Titulorr"
+    Title.Parent = Top
+    Title.BackgroundColor3 = TomPrimario
+    Title.BorderSizePixel = 0
+    Title.Position = UDim2.new(0, 0, 0, 0)
+    Title.Size = UDim2.new(0, Largura, 0, 25)
+    Title.Font = Enum.Font.GothamSemibold
+    Title.Text = "Super Hack do Niquistiniquis"
+    Title.TextColor3 = Color3.fromRGB(255, 255, 255)
+    Title.TextSize = 14.000
+    Title.TextWrapped = true
+
+    ContainerFunc.Name = "ContainerFuncoes"
+    ContainerFunc.Parent = Main
+    ContainerFunc.BackgroundColor3 = TomSecundario
+    ContainerFunc.BorderSizePixel = 1
+    ContainerFunc.BorderColor3 = Color3.fromRGB(10, 10, 10)
+    ContainerFunc.Position = UDim2.new(0, 5, 0, 31)
+    ContainerFunc.Size = UDim2.new(0, Largura-10, 0, Altura-36)   
+     
+    TopContainerFunc.Name = "TituloContainerFunc"
+    TopContainerFunc.Parent = ContainerFunc
+    TopContainerFunc.BackgroundColor3 = TomPrimario
+    TopContainerFunc.BorderSizePixel = 1
+    TopContainerFunc.BorderColor3 = Color3.fromRGB(10, 10, 10)
+    TopContainerFunc.Position = UDim2.new(0, 0, 0, 0)
+    TopContainerFunc.Size = UDim2.new(0, Largura-10, 0, 25)    
+
+    TitleJogo.Name = "TitulorrJogo"
+    TitleJogo.Parent = TopContainerFunc
+    TitleJogo.BackgroundColor3 = TomPrimario
+    TitleJogo.BorderSizePixel = 0
+    TitleJogo.Position = UDim2.new(0, 10, 0, 0)
+    TitleJogo.Size = UDim2.new(0, Largura-20, 0, 25)
+    TitleJogo.Font = Enum.Font.GothamSemibold
+    TitleJogo.Text = "Bee Factory Tycoon"
+    TitleJogo.TextColor3 = Color3.fromRGB(255, 255, 255)
+    TitleJogo.TextSize = 14.000
+    TitleJogo.TextWrapped = true
+    TitleJogo.TextXAlignment = Enum.TextXAlignment.Left
+
+    local TogCont = Instance.new("TextButton")
+    local ChkBox = Instance.new("Frame")
+    local redondar = Instance.new('UICorner')
+    local TogTitle = Instance.new("TextLabel")
+    local TogState = Instance.new("Frame")
+    local ButtonOnly = Instance.new("TextButton")
+
+    TogCont.Name = "TogCont"
+    TogCont.Parent = ContainerFunc
+    TogCont.BackgroundColor3 = TomSecundario
+    TogCont.BorderSizePixel = 0
+    TogCont.Position = UDim2.new(0, 5, 0, 31)
+    TogCont.Size = UDim2.new(0, Largura-20, 0, 25)
+    TogCont.Text = ""
+    TogCont.AutoButtonColor = false
+
+    ChkBox.Name = "ChkBoxTog"
+    ChkBox.Parent = TogCont
+    ChkBox.BackgroundColor3 = TomQuaternario
+    ChkBox.Position = UDim2.new(0, 0, 0, 0)
+    ChkBox.Size = UDim2.new(0, 25, 0, 25)  
+    redondar.Name = "ArredondaSaporra"
+    redondar.Parent = ChkBox
+    TogState.Name = "TogState"
+    TogState.Parent = ChkBox
+    TogState.BackgroundColor3 = TomSextenario
+    TogState.Position = UDim2.new(0, 2, 0, 2)
+    TogState.Size = UDim2.new(0, 21, 0, 21)
+    TogState.Visible = false
+    local redondar2 = redondar:Clone()
+    redondar2.Name = "ArredondaSaporra"
+    redondar2.Parent = TogState
+    
+    TogCont.MouseButton1Click:Connect(function()
+        if(ChkBox.TogState.Visible)then
+            ChkBox.TogState.Visible = false
+            config.entregar = false
+            config.upabieia = false
+            config.melzin = false
+            config.maitaxa = false
+            config.mashupa = false
+            config.antiafk = false
+        else
+            if not(ChkBox.TogState.Visible)then
+                ChkBox.TogState.Visible = true
+                config.entregar = true
+                config.upabieia = true
+                config.melzin = true
+                config.maitaxa = true
+                config.mashupa = true
+                config.antiafk = true
+            end
+        end
+    end)
+
+    TogTitle.Name = "TogTitle"
+    TogTitle.Parent = TogCont
+    TogTitle.BackgroundColor3 = TogCont.BackgroundColor3
+    TogTitle.BorderSizePixel = 0
+    TogTitle.Position = UDim2.new(0, 25+5, 0, 0)
+    TogTitle.Size = UDim2.new(0, Largura-20-25-5, 0, 25)
+    TogTitle.Font = Enum.Font.GothamSemibold
+    TogTitle.Text = "AutoFarm"
+    TogTitle.TextColor3 = Color3.fromRGB(255, 255, 255)
+    TogTitle.TextSize = 14.000
+    TogTitle.TextWrapped = true
+    TogTitle.TextXAlignment = Enum.TextXAlignment.Left
+
+    ButtonOnly.Name = "Matador"
+    ButtonOnly.Parent = ContainerFunc
+    ButtonOnly.BackgroundColor3 = ChkBox.BackgroundColor3
+    ButtonOnly.Position = UDim2.new(0, 5, 0, 31+25+5)
+    ButtonOnly.Size = UDim2.new(0, Largura-20, 0, 25)
+    ButtonOnly.BorderSizePixel = 1
+    ButtonOnly.BorderColor3 = Color3.fromRGB(10,10,10)
+    ButtonOnly.Text = "Fechar UI"
+    ButtonOnly.TextColor3 = Color3.fromRGB(255, 255, 255)
+    ButtonOnly.Font = Enum.Font.GothamSemibold
+    ButtonOnly.TextSize = 14.000
+    ButtonOnly.TextWrapped = true
+    ButtonOnly.MouseButton1Click:Connect(function()
         config.entregar = false
         config.upabieia = false
+        config.melzin = false
         config.maitaxa = false
         config.mashupa = false
         config.antiafk = false
-        KillMe()
-    end
-}
+        h:Destroy()
+    end)
+end
+CriarJanelaBase(300,128,Color3.new(0.227450, 0.188235, 0.050980))
